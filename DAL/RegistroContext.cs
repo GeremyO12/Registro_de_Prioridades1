546e8281ef29cb35945d8dba8ec4384e39f0db30
Geremy De los Santos S <geremiorlando28@hotmail.com>
2024-01-12 22:20:34 -0400

    Agrege la clase RegsitroContext

---
+++ DAL/RegistroContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_de_Prioridades1.Models;
+
+namespace Registro_de_Prioridades1.DAL
+{
+    public class RegistroContext : DbContext
+    {
+        public RegistroContext(DbContextOptions<RegistroContext> Options) 
+              : base(Options){ }
+        public DbSet<Sistemas> Sistemas { get; set;}
+    }
+}
