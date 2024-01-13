a8925395275b4e2f891fe6c9203c35039af93b4c
Geremy De los Santos S <geremiorlando28@hotmail.com>
2024-01-12 22:07:40 -0400

    Sistemas fue Agregado

---
+++ Models/Sistemas.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro_de_Prioridades1.Models
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemaId { get; set; }
+        [Required (ErrorMessage ="El nombre del sistema es obligatorio")]
+        public String? Nombre { get; set; }
+    }
+}
